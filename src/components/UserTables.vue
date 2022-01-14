df0623558c948ac7b65d7288a9bac22e6d3a967e
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-01-14 14:12:31 +0100

    wip: retrieve data from backend is possible

--- src/components/UserTables.vue
+++ src/components/UserTables.vue
@@ -1,25 +1,40 @@
 <script setup lang="ts">
-import data from '../assets/myMockedUserData.json'
-// import axios from '~/middleware/axios'
-const allUsers = reactive(data)
-const users = reactive(allUsers.filter(user => !user.isObserver))
-const observers = reactive(allUsers.filter(user => user.isObserver))
+import { collection, doc, getDocs, onSnapshot } from 'firebase/firestore'
+import type { Ref } from 'vue'
+// import getCollection from '~/composables/getCollection'
+import { db } from '~/firebase/config'
 
-// const { loading, result, error, callAPI } = useFireBaseApi(async (query: any) => {
-//   const res = await axios.get('URL')
-//   return res.data.message
-// })
+// const allUsers = reactive(data)
+// const observers = reactive(allUsers.filter(user => user.isObserver))
+
+// const { documents } = getCollection('users')
+
+const allUsers: Ref<string[]> = ref([])
+const colRef = collection(db, 'users')
+getDocs(colRef).then(onSnapshot => {
+  const docs: any[] = []
+  onSnapshot.docs.forEach(doc => {
+    docs.push({ ...doc.data() })
+  })
+  allUsers.value = docs
+})
+
+// map json to obect user
+
+// filter arrays and pass to table components
+// const users: User[] = reactive(allUsers.value.filter(user => !user.isObserver))
+// const observers: User[] = reactive(allUsers.value.filter(user => user.isObserver))
 </script>
 
 <template>
   <!-- <h2 v-if="error" class="error">Oops, something went wrong! Please try one more time</h2>
   <h2 v-if="loading">Loading...</h2>
   <div v-if="result"> -->
-    <EstimateTable :data="users">
-      <h1 class="my-4 prose lg:prose-xl">Users</h1>
-    </EstimateTable>
-    <EstimateTable :data="observers">
-      <h1 class="my-4 prose lg:prose-xl">Observers</h1>
-    </EstimateTable>
+  <EstimateTable :data="users">
+    <h1 class="my-4 prose lg:prose-xl">Users</h1>
+  </EstimateTable>
+  <EstimateTable :data="observers">
+    <h1 class="my-4 prose lg:prose-xl">Observers</h1>
+  </EstimateTable>
   <!-- </div> -->
 </template>
