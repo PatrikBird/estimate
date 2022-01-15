89df0fda31053b698802df83d3f68a5ccbd6178d
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-01-15 15:32:56 +0100

    feat: add user to db

--- src/components/InputModal.vue
+++ src/components/InputModal.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from '~/firebase/config'
 import { useMainStore } from '~/store/main'
 
 const mainStore = useMainStore()
 
 const enteredName = ref('')
-
 const formIsInvalid = ref(true)
 function validateForm() {
   if (enteredName.value !== '') {
@@ -12,21 +13,42 @@ function validateForm() {
     formIsInvalid.value = false
   }
 }
+
+const isObserver = ref(false)
+async function handleSubmit() {
+  validateForm()
+
+  const colRef = collection(db, 'users')
+  await addDoc(colRef, {
+    username: enteredName.value,
+    isObserver: isObserver.value,
+    hasVoted: false,
+  })
+}
 </script>
 
 <template>
   <input id="my-modal" type="checkbox" :checked="formIsInvalid" class="modal-toggle" />
   <div class="modal">
     <div class="modal-box">
-      <p>Enter a name</p>
+      <!-- <p>Enter a name</p> -->
       <div class="modal-action justify-center">
-        <form @submit.prevent="validateForm">
+        <form @submit.prevent="handleSubmit">
           <input
             v-model="enteredName"
             type="text"
-            placeholder="username"
+            placeholder="Your name"
             class="text-center input input-primary input-bordered" />
-          <button class="btn btn-primary ml-3" :disabled="enteredName == ''">Accept</button>
+          <div class="p-6 card bordered">
+            <div class="form-control">
+              <label class="cursor-pointer label">
+                <span class="label-text">Voter</span>
+                <input v-model="isObserver" type="checkbox" class="toggle toggle-primary mx-2" />
+                <span class="label-text">Observer</span>
+              </label>
+            </div>
+          </div>
+          <button class="btn btn-primary" :disabled="enteredName == ''">Accept</button>
         </form>
       </div>
     </div>
