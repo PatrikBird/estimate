d8c162fd3f97dd06fe77e323d8cffd4e150a78e1
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-01-15 09:27:09 +0100

    wip: check out ref behaviour
    document.value seems to be empty when I try to access it.
    logging the ref itself shows that it's not!

--- src/components/UserTables.vue
+++ src/components/UserTables.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
 import getCollection from '~/composables/getCollection'
 
-const { documents } = getCollection('users')
 
-// todo: arrays are empty. why?
-const users = documents.value.filter(u => u.isObserver === false)
-const observers = documents.value.filter(u => u.isObserver === true)
+const { document } = getCollection('users')
+
+console.log(document)
+console.log(document.value)
+
+// const test = document.
+
+const bla = document.value.filter(d => d.isObserver === true)
+
+
 </script>
 
 <template>
   <!-- <h2 v-if="error" class="error">Oops, something went wrong! Please try one more time</h2>
   <h2 v-if="loading">Loading...</h2>
   <div v-if="result"> -->
-  <EstimateTable :data="users">
+  <EstimateTable :data="bla">
     <h1 class="my-4 prose lg:prose-xl">Users</h1>
   </EstimateTable>
-  <EstimateTable :data="observers">
+  <EstimateTable :data="document">
     <h1 class="my-4 prose lg:prose-xl">Observers</h1>
   </EstimateTable>
   <!-- </div> -->
--- src/composables/getCollection.ts
+++ src/composables/getCollection.ts
@@ -5,36 +5,39 @@ import { collection, onSnapshot } from 'firebase/firestore'
 import { db } from '~/firebase/config'
 import type { User } from '~/types/User'
 
+function mapArray(arr: DocumentData[]): User[] {
+  return arr.map(doc => {
+    return {
+      id: doc.id,
+      username: doc.username,
+      hasVoted: doc.hasVoted,
+      isObserver: doc.isObserver,
+    }
+  })
+}
+
 const getCollection = (c: string) => {
-  const documents: Ref<User[]> = ref([])
+  const document: Ref<User[]> = ref([])
 
   // collection reference
   const colRef = collection(db, c)
 
   const unsub = onSnapshot(colRef, snapshot => {
     const results: DocumentData[] = []
+
     snapshot.docs.forEach(doc => {
       results.push({ ...doc.data(), id: doc.id })
     })
 
-    const users: User[] = results.map(doc => {
-      return {
-        id: doc.id,
-        username: doc.username,
-        hasVoted: doc.hasVoted,
-        isObserver: doc.isObserver,
-      }
-    })
-
     // update values
-    documents.value = users
+    document.value = mapArray(results)
   })
 
   watchEffect(onInvalidate => {
     onInvalidate(() => unsub())
   })
 
-  return { documents }
+  return { document }
 }
 
 export default getCollection
