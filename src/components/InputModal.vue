b45b924c32108fb577feff481e5385587dee282d
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-01-16 16:34:35 +0100

    chore: delete todo

--- src/components/InputModal.vue
+++ src/components/InputModal.vue
@@ -16,7 +16,6 @@ const isObserver = ref(false)
 async function handleSubmit() {
   validateForm()
   mainStore.addUserToDb(enteredName.value, isObserver.value)
-  // TODO: add user to localStorage
 }
 </script>
 
