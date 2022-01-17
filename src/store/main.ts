e77107641efc5545aad45aea3212e60e387b98b8
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-01-17 15:18:10 +0100

    fix: add username to browser store

--- src/store/main.ts
+++ src/store/main.ts
@@ -19,7 +19,7 @@ export const useMainStore = defineStore('main', () => {
    */
   const user: User = reactive({
     id: useLocalStorage('id', ''), // TODO: maybe use sessionStorage instead?
-    username: '', // FIXME: username not shown after F5
+    username: useLocalStorage('username', ''),
     vote: null,
     isObserver: false,
   })
