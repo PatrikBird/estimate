1738a087ee063922d18a80ae045d7785b6b91e08
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-02-01 16:47:56 +0100

    chore: remove chart libs and cleanup VoteResult

--- src/components/VoteResult.vue
+++ src/components/VoteResult.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-// import { BarChart } from 'vue-chart-3'
-// import { Chart, registerables } from 'chart.js'
 import type { User } from '~/types'
-
-// Chart.register(...registerables)
 
 const props = defineProps<{ users: User[] }>()
 
@@ -20,19 +16,8 @@ const reducedArray = computed(() => {
 // )
 // const labels = computed(() => sortedArray.value.map(({ username }) => username))
 // const votes = computed(() => sortedArray.value.map(({ vote }) => vote))
-
-// const chartData = {
-//   labels: labels.value,
-//   datasets: [
-//     {
-//       data: votes.value,
-//       backgroundColor: ['#33C4B0'],
-//     },
-//   ],
-// }
 </script>
 
 <template>
-  <!-- <BarChart :chart-data="chartData" /> -->
   <div></div>
 </template>
