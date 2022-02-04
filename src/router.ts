572a2c64bca67addf1c075a6ca83780f6444e73c
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-02-04 07:12:08 +0100

    feat: configure dynamic route

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Hero from '~/pages/Hero.vue'
+import Main from '~/pages/Main.vue'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/', component: Hero }],
+  routes: [
+    { path: '/', component: Hero },
+    {
+      path: '/:collectionId',
+      component: Main,
+    },
+  ],
 })
 
 export default router
