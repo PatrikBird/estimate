4078a223841672e2708366caec5442e03ca0fda3
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-06-07 15:15:26 +0900

    fix: hide tables if not logged in

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -26,10 +26,14 @@ provide('availableVotes', availableVotes)
 </script>
 
 <template>
-  <Overlay v-if="showOverlay" />
-  <NavBar />
-  <div class="overflow-x-auto max-w-xl m-5 mx-auto">
-    <Controls :available-votes="availableVotes"></Controls>
-    <Tables />
+  <div v-if="showOverlay">
+    <Overlay />
+  </div>
+  <div v-else>
+    <NavBar />
+    <div class="overflow-x-auto max-w-xl m-5 mx-auto">
+      <Controls :available-votes="availableVotes"></Controls>
+      <Tables />
+    </div>
   </div>
 </template>
