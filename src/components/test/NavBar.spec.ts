52574a6d2f0df9a0751d633c68941184a5bbb4b8
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-06-12 16:03:54 +0900

    test: refactor test and extend factory

--- src/components/test/NavBar.spec.ts
+++ src/components/test/NavBar.spec.ts
@@ -1,15 +1,15 @@
 import { createRouter, createMemoryHistory } from 'vue-router'
 import { routes } from '~/router'
-import { createTestingPinia } from '@pinia/testing'
 import { useMainStore } from '~/store/main'
+import { createTestingPinia, TestingOptions } from '@pinia/testing'
 import { shallowMount } from '@vue/test-utils'
 import NavBar from '../NavBar.vue'
 
-function factory() {
+function factory(options?: TestingOptions) {
   const router = createRouter({ routes, history: createMemoryHistory() })
   const wrapper = shallowMount(NavBar, {
     global: {
-      plugins: [router, createTestingPinia()],
+      plugins: [createTestingPinia(options), router],
     },
   })
   const store = useMainStore()
@@ -20,6 +20,23 @@ function factory() {
 describe('NavBar.vue', () => {
   it('displays whether user is an observer or a voter', () => {
     // GIVEN
+    const { wrapper, store } = factory({
+      initialState: {
+        user: {
+          id: '123456789',
+          username: 'peter lustig',
+          vote: null,
+          isObserver: false,
+        },
+      },
+    })
+    const btn = wrapper.find('[data-test="toggleObserverBtn"]')
+
+    // THEN
+    expect(wrapper.find('[data-test="toggleObserverBtn"]')).toBeTruthy()
+    expect(store.user.isObserver).toBe(false)
+    expect(btn.text()).toBe('Voter')
+  })
 
     // WHEN
     const { wrapper, store } = factory()
